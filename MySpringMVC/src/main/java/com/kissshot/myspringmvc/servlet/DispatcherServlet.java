8d1bf23bbcead30ec5a96993c74e893806a80356
kissshot <570546847@qq.com>
2018-09-30 20:00:34 +0800

    add myspringmvc

--- MySpringMVC/src/main/java/com/kissshot/myspringmvc/servlet/DispatcherServlet.java
+++ MySpringMVC/src/main/java/com/kissshot/myspringmvc/servlet/DispatcherServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,8 +29,21 @@ public class DispatcherServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String uri = req.getRequestURI();
+        String contextPath = req.getContextPath();
+        String path = uri.replace(contextPath, "");
+        Method method =  methodMap.get(uri);
+        if (method != null) {
+            try {
+                method.invoke(beanMap.get("/"+path.split("/")[1]));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
 
-    }
+        }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -49,7 +63,6 @@ public class DispatcherServlet extends HttpServlet {
             e.printStackTrace();
         }
         // ioc
-
         // 建立映射关系
         setMapping();
     }
@@ -59,10 +72,11 @@ public class DispatcherServlet extends HttpServlet {
             for (Map.Entry<String, Object> entry : beanMap.entrySet()) {
                 Class<?> aClass = entry.getValue().getClass();
                 if (aClass.isAnnotationPresent(Controller.class)) {
-                    Field[] fields = aClass.getFields();
-                    for (Field field : fields) {
-                        if (field.isAnnotationPresent(RequestMapping.class)) {
-//                            methodMap.put("",field.)
+                    Method[] methods = aClass.getMethods();
+                    for (Method method   : methods) {
+                        if (method.isAnnotationPresent(RequestMapping.class)) {
+                            RequestMapping methodPath = method.getAnnotation(RequestMapping.class);
+                            methodMap.put((entry.getKey() + methodPath.value()).replaceAll("//","/"),method);
                         }
                     }
                 }
@@ -75,7 +89,7 @@ public class DispatcherServlet extends HttpServlet {
         if (!beanMap.isEmpty()) {
             for (Map.Entry<String, Object> entry : beanMap.entrySet()) {
                 // 获取实例对象中所有属性
-                Field[] fields = entry.getValue().getClass().getFields();
+                Field[] fields = entry.getValue().getClass().getDeclaredFields();
                 for (Field field:fields) {
                     field.setAccessible(true);
                     if (field.isAnnotationPresent(AutoWrite.class)) {
